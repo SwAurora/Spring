ee7b9be10e71867203bc595e259b976ceb7faccd
rlatj <rlatjddnr74@gmail.com>
2022-08-02 10:32:19 +0900

    2022-08-02 master(ver1)

--- src/main/java/com/korea/mapper/TestMapper.java
+++ src/main/java/com/korea/mapper/TestMapper.java
@@ -1,11 +1,19 @@
 package com.korea.mapper;
 
+import com.korea.domain.BoardDTO;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public interface TestMapper
 {
     @Select("select sysdate() from dual")
     String getTime();
+
+    String getTime2();
+
+    @Select("select * from tbl_board")
+    List<BoardDTO> SelectAll();
 }
--- src/test/java/MyBatisSpring.java
+++ src/test/java/MyBatisSpring.java
@@ -1,37 +1,41 @@
+import com.korea.domain.BoardDTO;
 import com.korea.mapper.TestMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.sql.Connection;
+import java.util.List;
 
 @Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/applicationContext.xml")
-public class MybatisTests
+public class MyBatisSpring
 {
     @Autowired
-    private SqlSessionFactory ssf;
-    @Autowired
-    private TestMapper tm;
+    private TestMapper testMapper;
 
     @Test
     public void test()
     {
-        SqlSession dbs = ssf.openSession();
-        Connection conn = dbs.getConnection();
-        log.info("Conn's obj : " + conn);
+        String time = testMapper.getTime();
+        log.info("time : " + time);
+        log.info("TESTMAPPER : " + testMapper);
     }
 
     @Test
     public void test2()
     {
-        log.info(tm.getClass().getName());
-        log.info(tm.getTime());
+        String time2 = testMapper.getTime2();
+        // log.info("time2 : " + time2);
+    }
+
+    @Test
+    public void test3()
+    {
+        List<BoardDTO> list = testMapper.SelectAll();
+        // list.forEach(dto -> log.info(String.valueOf(dto)));
     }
 }
